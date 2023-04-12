b60136a1e7ae5d7badb039b43eded3b13fd0f072
ux-ui-pro <51360899+ux-ui-pro@users.noreply.github.com>
2023-04-12 09:15:08 +0300

    issue: test 120423-1

--- src/js/marquee-content.js
+++ src/js/marquee-content.js
@@ -13,181 +13,197 @@ const MarqueeContent = () => {
             dataDirection = marquee.dataset.mcDirection,
             dataMaxWidth = marquee.dataset.mcMax,
             dataMinWidth = marquee.dataset.mcMin,
-            currentClones = 0,
-            requiredClones = Math.ceil((marquee.scrollWidth / marquee.firstElementChild.clientWidth) + 2),
             breakpoint
 
         const
 
-            breakpoints = () => {
-                if(dataMaxWidth || dataMinWidth) {
-                    if(dataMaxWidth) {
-                        breakpoint = `(max-width: ${dataMaxWidth - 0.02}px)`
-                    } else if(dataMinWidth) {
-                        breakpoint = `(min-width: ${dataMinWidth}px)`
-                    }
-                } else {
-                    breakpoint = ``
+        debounce = (fn, delay) => {
+            let timer
+
+            return (...args) => {
+                if (timer) clearTimeout(timer)
+                timer = setTimeout(() => fn(...args), delay)
+            }
+        },
+
+        breakpoints = () => {
+            if(dataMaxWidth || dataMinWidth) {
+                if(dataMaxWidth) {
+                    breakpoint = `(max-width: ${dataMaxWidth - 0.02}px)`
+                } else if(dataMinWidth) {
+                    breakpoint = `(min-width: ${dataMinWidth}px)`
                 }
-            },
+            } else {
+                breakpoint = ``
+            }
+        },
 
-            templates = () => {
-                marquee.innerHTML  = `
-				<div class="marquee-wrapper">
-					<div class="marquee-inner" style="display: inline-block; white-space: nowrap;">${marquee.innerHTML}</div>
-				</div>
-			`
-            },
+        templates = () => {
+            marquee.innerHTML  = `
+                <div class="marquee-wrapper">
+                    <div class="marquee-inner" style="display: inline-block; white-space: nowrap;">${marquee.innerHTML}</div>
+                </div>
+            `
+        },
 
-            cloning = () => {
-                const removingClones = () => {
-                    while (marquee.children.length > 1) {
-                        marquee.removeChild(marquee.lastChild)
+        cloning = () => {
+            const removingClones = () => {
+                while (marquee.childElementCount > 1) {
+                    marquee.removeChild(marquee.lastChild)
+                }
+            }
+
+            removingClones()
+
+            matchMedia.add(breakpoint, () => {
+                let requiredQuantity = Math.ceil((marquee.scrollWidth / marquee.firstElementChild.clientWidth) + 2)
+
+                if (marquee.childElementCount < requiredQuantity) {
+                    for (let i = 1; i < (requiredQuantity); i++) {
+                        let cloned = marquee.firstElementChild
+                        let clone = cloned.cloneNode(true)
+                        cloned.parentNode.append(clone)
                     }
-
-                    currentClones = 0
                 }
 
-                removingClones()
+                return () => {
+                    removingClones()
+                }
+            })
+        },
 
-                matchMedia.add(breakpoint, () => {
-                    if (marquee.childElementCount > 0) {
+        skewed = () => {
+            if(!dataSkew) return
 
-                        if (currentClones < requiredClones) {
-                            for (let i = currentClones; i < (requiredClones); i++) {
-                                let cloned = marquee.firstElementChild
-                                let clone = cloned.cloneNode(true)
-                                cloned.parentNode.append(clone)
-                            }
+            matchMedia.add(breakpoint, () => {
+                let abs = (dataSkew < 0) ? dataSkew * -1 : dataSkew
 
-                            currentClones = requiredClones
+                marquee.style.transformOrigin = 'center center'
+                marquee.style.transform = `skew(0deg, ${dataSkew}deg)`
+                marquee.style.minHeight = `calc(${abs * 1.25}vh + ${abs * 1.25}vw)`
+
+                return () => {
+                    marquee.style.removeProperty('transform-origin')
+                    marquee.style.removeProperty('transform')
+                    marquee.style.removeProperty('min-height')
+                }
+            })
+        },
+
+        animation = () => {
+            matchMedia.add(breakpoint, () => {
+                gsap.set(marquee.children, { 'will-change': 'transform'	})
+
+                const tween = gsap.to(marquee.children, {
+                    duration: dataDuration,
+                    x: '-100%',
+                    ease: 'none',
+                    repeat: -1,
+                    scrollTrigger: {
+                        trigger: marquee,
+                        start: '-=50% bottom',
+                        end: 'bottom top',
+                        toggleActions: 'resume pause resume pause'
+                    },
+                }).totalProgress(0.5)
+
+                const ltrDirection = () => {
+                    gsap.to(tween, {
+                        timeScale: -1,
+                        overwrite: true
+                    })
+                }
+
+                const autoDirection = () => {
+                    let previousScrollPosition = 0
+
+                    const isScrollingDown = () => {
+                        let scrollPosition = window.pageYOffset || document.documentElement.scrollTop
+
+                        let direction = false
+
+                        if (scrollPosition > previousScrollPosition) {
+                            direction = true
                         }
-                    }
 
-                    return () => {
-                        removingClones()
-                    }
-                })
-            },
+                        previousScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition
 
-            skewed = () => {
-                if(!dataSkew) return
-
-                matchMedia.add(breakpoint, () => {
-                    let abs = (dataSkew < 0) ? dataSkew * -1 : dataSkew
-
-                    marquee.style.transformOrigin = 'center center'
-                    marquee.style.transform = `skew(0deg, ${dataSkew}deg)`
-                    marquee.style.minHeight = `calc(${abs * 1.25}vh + ${abs * 1.25}vw)`
-
-                    return () => {
-                        marquee.style.removeProperty('transform-origin')
-                        marquee.style.removeProperty('transform')
-                        marquee.style.removeProperty('min-height')
-                    }
-                })
-            },
-
-            animation = () => {
-                matchMedia.add(breakpoint, () => {
-                    gsap.set(marquee.children, { 'will-change': 'transform'	})
-
-                    const tween = gsap.to(marquee.children, {
-                        duration: dataDuration,
-                        x: '-100%',
-                        ease: 'none',
-                        repeat: -1,
-                        scrollTrigger: {
-                            trigger: marquee,
-                            start: '-=50% bottom',
-                            end: 'bottom top',
-                            toggleActions: 'resume pause resume pause'
-                        },
-                    }).totalProgress(0.5)
-
-                    const ltrDirection = () => {
                         gsap.to(tween, {
-                            timeScale: -1,
+                            timeScale: direction ? -1 : 1,
                             overwrite: true
                         })
                     }
 
-                    const autoDirection = () => {
-                        let previousScrollPosition = 0
-
-                        const isScrollingDown = () => {
-                            let scrollPosition = window.pageYOffset || document.documentElement.scrollTop
-
-                            let direction = false
-
-                            if (scrollPosition > previousScrollPosition) {
-                                direction = true
-                            }
-
-                            previousScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition
-
-                            gsap.to(tween, {
-                                timeScale: direction ? -1 : 1,
-                                overwrite: true
-                            })
-                        }
-
-                        addEventListener('scroll', isScrollingDown, {
-                            capture: true,
-                            passive: true
-                        })
-                    }
-
-                    if(dataDirection === 'ltr') {
-                        ltrDirection()
-                    } else if(dataDirection === 'auto') {
-                        autoDirection()
-                    }
-                })
-            },
-
-            killAnimation = () => {
-                let timeLine = gsap.timeline()
-
-                timeLine.kill()
-                timeLine = null
-
-                gsap.set(marquee.children, { clearProps: true })
-            },
-
-            update = () => {
-                skewed()
-                cloning()
-                killAnimation()
-                animation()
-            },
-
-            init = () => {
-                breakpoints()
-                templates()
-
-                matchMedia.add('(any-pointer: coarse)', () => {
-                    let portrait = window.matchMedia('(orientation: portrait)')
-
-                    portrait.addEventListener('change', (e) => {
-                        if(!e.matches) {
-                            update()
-                        }
+                    addEventListener('scroll', isScrollingDown, {
+                        capture: true,
+                        passive: true
                     })
-                })
-
-                matchMedia.add('(any-pointer: fine)', () => {
-                    addEventListener('resize', () => {
-                        update()
-                    })
-                })
-
-                try {
-                    document.fonts.ready.then(() => update())
-                } catch (error) {
-                    update()
                 }
-            }
+
+                if(dataDirection === 'ltr') {
+                    ltrDirection()
+                } else if(dataDirection === 'auto') {
+                    autoDirection()
+                }
+            })
+        },
+
+        killAnimation = () => {
+            let timeLine = gsap.timeline()
+
+            timeLine.kill()
+            timeLine = null
+
+            gsap.set(marquee.children, { clearProps: true })
+        },
+
+        update = () => {
+            skewed()
+            cloning()
+            killAnimation()
+            animation()
+        },
+
+        init = () => {
+            breakpoints()
+            templates()
+
+            // addEventListener('resize', debounce(() => {
+            //     update()
+            // }, 150))
+
+            const eventListener = window.matchMedia && window.matchMedia('(pointer: coarse)').matches
+                ? window.matchMedia('(orientation: portrait)')
+                : window
+
+            eventListener.addEventListener('change', debounce((e) => {
+                !e.matches && update()
+            }, 150))
+
+            window.addEventListener('resize', debounce(() => {
+                update()
+            }, 150))
+
+            // if (window.matchMedia && window.matchMedia('(pointer: coarse)').matches) {
+            //     let portrait = window.matchMedia('(orientation: portrait)')
+            //
+            //     portrait.addEventListener('change', debounce((e) => {
+            //         if(!e.matches) {
+            //             update()
+            //         }
+            //     }, 150))
+            // } else {
+            //     addEventListener('resize', debounce(() => {
+            //         update()
+            //     }, 150))
+            // }
+
+            document.fonts.ready.then(() => {
+                update()
+            }).catch(() => {
+                update()
+            })
+        }
 
         init()
     }
