dff23fb335eb83004e0a0163d7b09a259dd3ec69
ux-ui-pro <maxim@ux-ui.pro>
2024-06-14 00:41:08 +0300

    issue: parade of shame

--- dist/index.js
+++ dist/index.js
@@ -9,78 +9,67 @@ function $parcel$export(e, n, v, s) {
 
 $parcel$defineInteropFlag(module.exports);
 
-$parcel$export(module.exports, "default", function () { return $a196c1ed25598f0e$export$2e2bcd8739ae039; });
-class $a196c1ed25598f0e$var$MarqueeContent {
-    gsap;
-    MM;
-    timeline;
-    element;
-    resizeObserver;
-    animationFrame = null;
-    static gsap;
-    static ScrollTrigger;
+$parcel$export(module.exports, "default", function () { return $6be4b30feeb09703$export$2e2bcd8739ae039; });
+class $6be4b30feeb09703$var$MarqueeContent {
+    #gsap;
+    #MM;
+    #timeline;
+    #element;
+    #resizeObserver;
+    #animationFrame;
     constructor({ element: element = ".marquee" } = {}){
-        this.gsap = $a196c1ed25598f0e$var$MarqueeContent.gsap ?? window.gsap;
-        this.MM = this.gsap.matchMedia();
-        this.timeline = null;
-        this.element = typeof element === "string" ? document.querySelector(element) : element;
-        if (!this.element) return;
-        this.resizeObserver = new ResizeObserver($a196c1ed25598f0e$var$MarqueeContent.debounce(this.update));
-        this.resizeObserver.observe(this.element);
+        this.#gsap = $6be4b30feeb09703$var$MarqueeContent.gsap || window.gsap;
+        this.#MM = this.#gsap.matchMedia();
+        this.#timeline = null;
+        this.#element = element instanceof HTMLElement ? element : document.querySelector(element);
+        if (!this.#element) return;
+        this.#resizeObserver = new ResizeObserver($6be4b30feeb09703$var$MarqueeContent.#debounce(this.#update.bind(this)));
+        this.#resizeObserver.observe(this.#element);
     }
     static registerGSAP(gsap, ScrollTrigger) {
-        $a196c1ed25598f0e$var$MarqueeContent.gsap = gsap;
-        $a196c1ed25598f0e$var$MarqueeContent.ScrollTrigger = ScrollTrigger;
+        $6be4b30feeb09703$var$MarqueeContent.gsap = gsap;
+        $6be4b30feeb09703$var$MarqueeContent.ScrollTrigger = ScrollTrigger;
     }
-    commonInit = ()=>{
-        if (this.element) {
-            this.clearTimeline();
-            this.cloneElements();
-            this.setBreakpoints();
-            this.applySkew();
-            this.timeline = this.createAnimation();
-        }
-    };
-    update = ()=>{
-        if (this.animationFrame !== null) cancelAnimationFrame(this.animationFrame);
-        this.animationFrame = requestAnimationFrame(()=>{
-            this.commonInit();
-            $a196c1ed25598f0e$var$MarqueeContent.ScrollTrigger.refresh();
-        });
-    };
-    init = ()=>{
-        this.commonInit();
-    };
-    destroy = ()=>{
-        if (this.animationFrame !== null) cancelAnimationFrame(this.animationFrame);
-        if (this.element) this.clearTimeline();
-        this.resizeObserver?.disconnect();
-    };
-    static debounce(func) {
+    static #debounce(func) {
         let timer = null;
         return (...args)=>{
             if (timer !== null) cancelAnimationFrame(timer);
             timer = requestAnimationFrame(()=>func(...args));
         };
     }
-    createAnimation() {
-        this.timeline?.kill();
-        const timeline = this.gsap.timeline({
+    #commonInit() {
+        if (this.#element) {
+            this.#clearTimeline();
+            this.#cloneElements();
+            this.#setBreakpoints();
+            this.#applySkew();
+            this.#timeline = this.#createAnimation();
+        }
+    }
+    #update() {
+        if (this.#animationFrame !== null) cancelAnimationFrame(this.#animationFrame);
+        this.#animationFrame = requestAnimationFrame(()=>{
+            this.#commonInit();
+            $6be4b30feeb09703$var$MarqueeContent.ScrollTrigger.refresh();
+        });
+    }
+    #createAnimation() {
+        this.#timeline?.kill();
+        const timeline = this.#gsap.timeline({
             scrollTrigger: {
-                trigger: this.element,
+                trigger: this.#element,
                 start: "top bottom",
                 end: "bottom top",
                 toggleActions: "resume pause resume pause",
                 onUpdate: (self)=>{
-                    if (this.element?.dataset.mcDirection === "ltr") timeline.timeScale(-1);
-                    else if (this.element?.dataset.mcDirection === "auto") timeline.timeScale(self.direction);
+                    if (this.#element?.dataset.mcDirection === "ltr") timeline.timeScale(-1);
+                    else if (this.#element?.dataset.mcDirection === "auto") timeline.timeScale(self.direction);
                 }
             }
         });
-        this.MM.add(this.element?.dataset.mcBreakpoint, ()=>{
-            const mcSpeed = parseFloat(this.element?.dataset.mcSpeed || "20");
-            const speed = mcSpeed;
-            timeline.to(this.element.children, {
+        this.#MM.add(this.#element?.dataset.mcBreakpoint, ()=>{
+            const speed = parseFloat(this.#element?.dataset.mcSpeed || "20");
+            timeline.to(this.#element.children, {
                 duration: speed,
                 x: "-100%",
                 repeat: -1,
@@ -91,44 +80,52 @@ class $a196c1ed25598f0e$var$MarqueeContent {
         });
         return timeline;
     }
-    setBreakpoints() {
-        if (this.element.dataset.mcMax) this.element.dataset.mcBreakpoint = `(max-width: ${parseFloat(this.element.dataset.mcMax) - 0.02}px)`;
-        else if (this.element.dataset.mcMin) this.element.dataset.mcBreakpoint = `(min-width: ${this.element.dataset.mcMin}px)`;
-        else this.element.dataset.mcBreakpoint = "";
+    #setBreakpoints() {
+        if (this.#element.dataset.mcMax) this.#element.dataset.mcBreakpoint = `(max-width: ${parseFloat(this.#element.dataset.mcMax) - 0.02}px)`;
+        else if (this.#element.dataset.mcMin) this.#element.dataset.mcBreakpoint = `(min-width: ${this.#element.dataset.mcMin}px)`;
+        else this.#element.dataset.mcBreakpoint = "";
     }
-    clearTimeline() {
-        this.timeline?.kill();
-        this.timeline = null;
-        this.gsap.set(this.element.children, {
+    #clearTimeline() {
+        this.#timeline?.kill();
+        this.#timeline = null;
+        this.#gsap.set(this.#element.children, {
             clearProps: "all"
         });
     }
-    cloneElements() {
+    #cloneElements() {
         const removingClones = ()=>{
-            while(this.element.childElementCount > 1)this.element.removeChild(this.element.lastChild);
+            while(this.#element.childElementCount > 1)this.#element.removeChild(this.#element.lastChild);
         };
         removingClones();
-        this.MM.add(this.element.dataset.mcBreakpoint, ()=>{
-            const requiredQuantity = Math.ceil(this.element.scrollWidth / this.element.firstElementChild.clientWidth + 2);
-            if (this.element.childElementCount < requiredQuantity) {
+        this.#MM.add(this.#element.dataset.mcBreakpoint, ()=>{
+            const requiredQuantity = Math.ceil(this.#element.scrollWidth / this.#element.firstElementChild.clientWidth + 2);
+            if (this.#element.childElementCount < requiredQuantity) {
                 const fragment = document.createDocumentFragment();
                 const clones = Array.from({
                     length: requiredQuantity - 1
-                }, ()=>this.element.firstElementChild.cloneNode(true));
+                }, ()=>this.#element.firstElementChild.cloneNode(true));
                 clones.forEach((clone)=>fragment.appendChild(clone));
-                this.element.appendChild(fragment);
+                this.#element.appendChild(fragment);
             }
             return removingClones;
         });
     }
-    applySkew() {
-        if (!this.element?.dataset.mcSkew) return;
-        const { style: style } = this.element;
+    #applySkew() {
+        if (!this.#element?.dataset.mcSkew) return;
+        const { style: style } = this.#element;
         style.transformOrigin = "center center";
-        style.transform = `skew(0deg, ${this.element.dataset.mcSkew}deg)`;
+        style.transform = `skew(0deg, ${this.#element.dataset.mcSkew}deg)`;
+    }
+    init() {
+        this.#commonInit();
+    }
+    destroy() {
+        if (this.#animationFrame !== null) cancelAnimationFrame(this.#animationFrame);
+        if (this.#element) this.#clearTimeline();
+        this.#resizeObserver?.disconnect();
     }
 }
-var $a196c1ed25598f0e$export$2e2bcd8739ae039 = $a196c1ed25598f0e$var$MarqueeContent;
+var $6be4b30feeb09703$export$2e2bcd8739ae039 = $6be4b30feeb09703$var$MarqueeContent;
 
 
 //# sourceMappingURL=index.js.map
--- dist/index.module.js
+++ dist/index.module.js
@@ -1,74 +1,63 @@
-class $643fcf18b2d2e76f$var$MarqueeContent {
-    gsap;
-    MM;
-    timeline;
-    element;
-    resizeObserver;
-    animationFrame = null;
-    static gsap;
-    static ScrollTrigger;
+class $c38b013c361dbfdf$var$MarqueeContent {
+    #gsap;
+    #MM;
+    #timeline;
+    #element;
+    #resizeObserver;
+    #animationFrame;
     constructor({ element: element = ".marquee" } = {}){
-        this.gsap = $643fcf18b2d2e76f$var$MarqueeContent.gsap ?? window.gsap;
-        this.MM = this.gsap.matchMedia();
-        this.timeline = null;
-        this.element = typeof element === "string" ? document.querySelector(element) : element;
-        if (!this.element) return;
-        this.resizeObserver = new ResizeObserver($643fcf18b2d2e76f$var$MarqueeContent.debounce(this.update));
-        this.resizeObserver.observe(this.element);
+        this.#gsap = $c38b013c361dbfdf$var$MarqueeContent.gsap || window.gsap;
+        this.#MM = this.#gsap.matchMedia();
+        this.#timeline = null;
+        this.#element = element instanceof HTMLElement ? element : document.querySelector(element);
+        if (!this.#element) return;
+        this.#resizeObserver = new ResizeObserver($c38b013c361dbfdf$var$MarqueeContent.#debounce(this.#update.bind(this)));
+        this.#resizeObserver.observe(this.#element);
     }
     static registerGSAP(gsap, ScrollTrigger) {
-        $643fcf18b2d2e76f$var$MarqueeContent.gsap = gsap;
-        $643fcf18b2d2e76f$var$MarqueeContent.ScrollTrigger = ScrollTrigger;
+        $c38b013c361dbfdf$var$MarqueeContent.gsap = gsap;
+        $c38b013c361dbfdf$var$MarqueeContent.ScrollTrigger = ScrollTrigger;
     }
-    commonInit = ()=>{
-        if (this.element) {
-            this.clearTimeline();
-            this.cloneElements();
-            this.setBreakpoints();
-            this.applySkew();
-            this.timeline = this.createAnimation();
-        }
-    };
-    update = ()=>{
-        if (this.animationFrame !== null) cancelAnimationFrame(this.animationFrame);
-        this.animationFrame = requestAnimationFrame(()=>{
-            this.commonInit();
-            $643fcf18b2d2e76f$var$MarqueeContent.ScrollTrigger.refresh();
-        });
-    };
-    init = ()=>{
-        this.commonInit();
-    };
-    destroy = ()=>{
-        if (this.animationFrame !== null) cancelAnimationFrame(this.animationFrame);
-        if (this.element) this.clearTimeline();
-        this.resizeObserver?.disconnect();
-    };
-    static debounce(func) {
+    static #debounce(func) {
         let timer = null;
         return (...args)=>{
             if (timer !== null) cancelAnimationFrame(timer);
             timer = requestAnimationFrame(()=>func(...args));
         };
     }
-    createAnimation() {
-        this.timeline?.kill();
-        const timeline = this.gsap.timeline({
+    #commonInit() {
+        if (this.#element) {
+            this.#clearTimeline();
+            this.#cloneElements();
+            this.#setBreakpoints();
+            this.#applySkew();
+            this.#timeline = this.#createAnimation();
+        }
+    }
+    #update() {
+        if (this.#animationFrame !== null) cancelAnimationFrame(this.#animationFrame);
+        this.#animationFrame = requestAnimationFrame(()=>{
+            this.#commonInit();
+            $c38b013c361dbfdf$var$MarqueeContent.ScrollTrigger.refresh();
+        });
+    }
+    #createAnimation() {
+        this.#timeline?.kill();
+        const timeline = this.#gsap.timeline({
             scrollTrigger: {
-                trigger: this.element,
+                trigger: this.#element,
                 start: "top bottom",
                 end: "bottom top",
                 toggleActions: "resume pause resume pause",
                 onUpdate: (self)=>{
-                    if (this.element?.dataset.mcDirection === "ltr") timeline.timeScale(-1);
-                    else if (this.element?.dataset.mcDirection === "auto") timeline.timeScale(self.direction);
+                    if (this.#element?.dataset.mcDirection === "ltr") timeline.timeScale(-1);
+                    else if (this.#element?.dataset.mcDirection === "auto") timeline.timeScale(self.direction);
                 }
             }
         });
-        this.MM.add(this.element?.dataset.mcBreakpoint, ()=>{
-            const mcSpeed = parseFloat(this.element?.dataset.mcSpeed || "20");
-            const speed = mcSpeed;
-            timeline.to(this.element.children, {
+        this.#MM.add(this.#element?.dataset.mcBreakpoint, ()=>{
+            const speed = parseFloat(this.#element?.dataset.mcSpeed || "20");
+            timeline.to(this.#element.children, {
                 duration: speed,
                 x: "-100%",
                 repeat: -1,
@@ -79,45 +68,53 @@ class $643fcf18b2d2e76f$var$MarqueeContent {
         });
         return timeline;
     }
-    setBreakpoints() {
-        if (this.element.dataset.mcMax) this.element.dataset.mcBreakpoint = `(max-width: ${parseFloat(this.element.dataset.mcMax) - 0.02}px)`;
-        else if (this.element.dataset.mcMin) this.element.dataset.mcBreakpoint = `(min-width: ${this.element.dataset.mcMin}px)`;
-        else this.element.dataset.mcBreakpoint = "";
+    #setBreakpoints() {
+        if (this.#element.dataset.mcMax) this.#element.dataset.mcBreakpoint = `(max-width: ${parseFloat(this.#element.dataset.mcMax) - 0.02}px)`;
+        else if (this.#element.dataset.mcMin) this.#element.dataset.mcBreakpoint = `(min-width: ${this.#element.dataset.mcMin}px)`;
+        else this.#element.dataset.mcBreakpoint = "";
     }
-    clearTimeline() {
-        this.timeline?.kill();
-        this.timeline = null;
-        this.gsap.set(this.element.children, {
+    #clearTimeline() {
+        this.#timeline?.kill();
+        this.#timeline = null;
+        this.#gsap.set(this.#element.children, {
             clearProps: "all"
         });
     }
-    cloneElements() {
+    #cloneElements() {
         const removingClones = ()=>{
-            while(this.element.childElementCount > 1)this.element.removeChild(this.element.lastChild);
+            while(this.#element.childElementCount > 1)this.#element.removeChild(this.#element.lastChild);
         };
         removingClones();
-        this.MM.add(this.element.dataset.mcBreakpoint, ()=>{
-            const requiredQuantity = Math.ceil(this.element.scrollWidth / this.element.firstElementChild.clientWidth + 2);
-            if (this.element.childElementCount < requiredQuantity) {
+        this.#MM.add(this.#element.dataset.mcBreakpoint, ()=>{
+            const requiredQuantity = Math.ceil(this.#element.scrollWidth / this.#element.firstElementChild.clientWidth + 2);
+            if (this.#element.childElementCount < requiredQuantity) {
                 const fragment = document.createDocumentFragment();
                 const clones = Array.from({
                     length: requiredQuantity - 1
-                }, ()=>this.element.firstElementChild.cloneNode(true));
+                }, ()=>this.#element.firstElementChild.cloneNode(true));
                 clones.forEach((clone)=>fragment.appendChild(clone));
-                this.element.appendChild(fragment);
+                this.#element.appendChild(fragment);
             }
             return removingClones;
         });
     }
-    applySkew() {
-        if (!this.element?.dataset.mcSkew) return;
-        const { style: style } = this.element;
+    #applySkew() {
+        if (!this.#element?.dataset.mcSkew) return;
+        const { style: style } = this.#element;
         style.transformOrigin = "center center";
-        style.transform = `skew(0deg, ${this.element.dataset.mcSkew}deg)`;
+        style.transform = `skew(0deg, ${this.#element.dataset.mcSkew}deg)`;
+    }
+    init() {
+        this.#commonInit();
+    }
+    destroy() {
+        if (this.#animationFrame !== null) cancelAnimationFrame(this.#animationFrame);
+        if (this.#element) this.#clearTimeline();
+        this.#resizeObserver?.disconnect();
     }
 }
-var $643fcf18b2d2e76f$export$2e2bcd8739ae039 = $643fcf18b2d2e76f$var$MarqueeContent;
+var $c38b013c361dbfdf$export$2e2bcd8739ae039 = $c38b013c361dbfdf$var$MarqueeContent;
 
 
-export {$643fcf18b2d2e76f$export$2e2bcd8739ae039 as default};
+export {$c38b013c361dbfdf$export$2e2bcd8739ae039 as default};
 //# sourceMappingURL=index.module.js.map
